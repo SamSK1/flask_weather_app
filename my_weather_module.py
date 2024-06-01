333b71314d997fc407eaea22b659eba59f294691
SamSk <sskurikhin3@gmail.com>
2024-06-01 12:16:56 +0200

    adding weather module

---
+++ my_weather_module.py
@@ -0,0 +1,20 @@
+import os 
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+
+load_dotenv()
+def get_current_weather(city='Zurich'):
+    
+    weather_api_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+    weather_data=requests.get(weather_api_url).json()
+    return weather_data
+
+if __name__=="__main__":
+    print("***entered weather module***")
+    city=input(f"\nPlease enter city name: ")
+    
+    weather_data=get_current_weather(city)
+
+    print("****")
+    pprint(weather_data)
