cf91a05f418c2e9089e47ea788ddde5acf5c22ff
SamSk <sskurikhin3@gmail.com>
2024-06-22 19:23:20 +0200

    adding fucntionality to the random button and handling error codes

--- my_weather_module.py
+++ my_weather_module.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import random
 import requests
-
+import pycountry_convert as pc
 load_dotenv()
 def get_current_weather(city='Zurich'):
     
@@ -21,13 +21,16 @@ def get_random_weather():
     get_country_code=int(random.randint(0,len(countries_api['data'])-1))
     get_country=countries_api['data'][get_country_code]['country']
     get_city=random.choice(countries_api['data'][get_country_code]['cities'])
+    weather_data=get_current_weather(get_city)
 
-    weather_api_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={get_city}&units=metric'
-    weather_data=requests.get(weather_api_url).json()
+    # return weather_data
+
+    # country_alpha2=weather_data['sys']['country']
+    # country_name=pc.country_alpha2_to_country_name(country_alpha2)
     return weather_data
 if __name__=="__main__":
     print("***entered weather module***")
     
-    get_random_weather()
-    
+    print(get_random_weather())
+
     print("****")
--- server.py
+++ server.py
@@ -3,8 +3,9 @@ from my_weather_module import get_current_weather
 from my_weather_module import get_weather_picture
 from my_weather_module import get_random_weather
 from waitress import serve
+import pycountry_convert as pc
 import webbrowser
-
+import random
 app=Flask(__name__)
 
 @app.route('/')
@@ -29,11 +30,13 @@ def get_weather():
         return render_template('city_not_found.html')
     weather_data=get_current_weather(city)
     weather_picture=get_weather_picture(city)
+    country_alpha2=weather_data['sys']['country']
+    country=pc.country_alpha2_to_country_name(country_alpha2)
 
 
     return render_template(
         "weather.html",
-        title=weather_data['name'],
+        title=str(weather_data['name'])+","+country,
         status=weather_data["weather"][0]["description"].capitalize(),
         temp=f"{weather_data['main']['temp']:.1f}",
         feels_like=f"{weather_data['main']['feels_like']:.1f}",
@@ -43,9 +46,34 @@ def get_weather():
 
 @app.route('/random_weather')
 def get_random_weather_city():
-    weather_data=get_random_weather()
+
+    
+    
+    try:
+            
+            weather_data=get_random_weather()
+            city=weather_data['name']
+    except:
+            emergency_city=['Paris','London','Chur','Bologna','Hamburg','Bratislava']
+            city=random.choice(emergency_city)
+            
+    country_alpha2=weather_data['sys']['country']
+    if country_alpha2=='XK':
+         country=pc.country_alpha2_to_country_name('US')
+         city='Chicago'
+  
+    country=pc.country_alpha2_to_country_name(country_alpha2)
+    weather_data=get_current_weather(city)
+    weather_picture=get_weather_picture(city)
     
 
+    return render_template(        
+        "weather.html",
+        title=str(weather_data['name'])+","+country,
+        status=weather_data["weather"][0]["description"].capitalize(),
+        temp=f"{weather_data['main']['temp']:.1f}",
+        feels_like=f"{weather_data['main']['feels_like']:.1f}",
+        image=weather_picture)
 if __name__=="__main__":
     # serve(app,host="0.0.0.0", port=5000)
     app.run(debug=True,port=5000, host='0.0.0.0')
